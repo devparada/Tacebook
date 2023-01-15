d2fd2c44bd70df3b66aaf9c4a77e19539a7234f7
803Dyse <alexdyse803@gmail.com>
2023-01-15 18:21:18 +0100

    (Incompleto) creados los constructores de comment y post.

--- src/tacebook/Comment.java
+++ src/tacebook/Comment.java
@@ -3,13 +3,47 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package tacebook;
+
 import java.util.Date;
+
 /**
  *
  * @author Bilo Alejandro Martins Gonzalez
  */
 public class Comment {
+
     private int id;
     private Date date;
     private String text;
+
+    public Comment(int id, Date date, String text) {
+        this.id = id;
+        this.date = date;
+        this.text = text;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
 }
