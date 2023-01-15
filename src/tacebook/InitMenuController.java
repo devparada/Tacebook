7c74f19cdf86cdc21dae821f1029c7a7f538e3a7
803Dyse <alexdyse803@gmail.com>
2023-01-15 17:50:46 +0100

    (Incompleto) Creacion de las clases con las que utilizaremos

---
+++ src/tacebook/InitMenuController.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tacebook;
+
+/**
+ *
+ * @author Bilo Alejandro Martins Gonzalez
+ */
+public class InitMenuController {
+    public static void main(String[] args) {
+        
+    }
+}
