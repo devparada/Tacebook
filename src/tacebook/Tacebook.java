5b64d52ead6e9f01629db6be6c787a198be561b1
SrUsuario <raul.parada040103@gmail.com>
2023-01-15 17:36:08 +0100

    Create Proyect Tacebook

---
+++ src/tacebook/Tacebook.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tacebook;
+
+/**
+ *
+ * @author Bilo Alejandro Martins Gonzalez y Raul Parada de la Fuente
+ */
+public class Tacebook {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
