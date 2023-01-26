c91e00aa2b78d1a34dc0e031d005557a49151b6c
bmargon <bmargon@DAW1-41.dip.local>
2023-01-26 09:38:54 +0100

    (Completo)Clase ProfileDB  todo corregido, comentado e implementado

--- src/tacebook/ProfileDB.java
+++ src/tacebook/ProfileDB.java
@@ -5,12 +5,26 @@
 package tacebook;
 
 /**
+ * Esta clase va acceder, filtrar y guardar los datos de nuestro programa en
+ * función de los perfiles, que son aqui los objetos 'profile'. Para su
+ * funcionalidad utilizaremos los metodos findByName, findByNameAndPassword,
+ * save y update.
  *
  * @author Bilo Alejandro Martins González y Raúl Parada de la Fuente
  */
 public class ProfileDB {
 
-    //!!!Revisar puede que esté mal!!!
+    /**
+     * Este método percorre el arrayList profile de la clase TacebookDB,
+     * haciendo un filtrado para obtener la cantidad de posts de cada perfil,
+     * haciendo llamada al getter de nuestro arrayList, con solo llamar el name,
+     * ya obtenemos como parametro el atributo que almancena la cantidad de
+     * posts
+     *
+     * @param name nnombre del perfil (la persona en concreto)
+     * @param numberOfPosts atributo que guarda la cantidad de posts del usario
+     * @return profile
+     */
     public static Profile findByName(String name, int numberOfPosts) {
         for (Profile profile : TacebookDB.getProfiles()) {
             if (profile.getName().equals(name)) {
@@ -21,38 +35,43 @@ public class ProfileDB {
     }
 
     /*
-    TODO: este metodo pide el nombre y la contraseña, pero al llamar al nombre,
-    probé utilizar el metodo findbyname, que retorna nombre, puede que sirva y 
-    puede que no, por lo tanto este comentario lo hice para avisar que si falla 
-    algo, seguramente hay que cambiar algo de caracter name, en la condicion if 
-    por ejemplo
+    Este metodo, igual que findByName pero, con operador and porque quieres mas 
+    datos, implementaremos la recuperacion de profile pero con name, password y 
+    number of posts
      */
     public static Profile findByNameAndPassword(String name, String password, int numberOfPosts) {
+        //Enhanced for para percorrer todos los objetos profile de TacebookDB
         for (Profile profile : TacebookDB.getProfiles()) {
-            if (profile.getName().equals(findByName(name, numberOfPosts))) {
-
+            //Es exactamente los mismo que el otro método pero añadiendo un 
+            //operador && para hacer llamada a getPassword
+            if (profile.getName().equals(name) && profile.getPassword().equals(password)) {
+                //Una vez percorrido todo el arrayList "profile", ya podemos 
+                //retornar nuestro objeto profile, ahora ya está filtrado
+                return profile;
             }
         }
         return null;
     }
 
-    /*
-    TODO: No sé se está bien, por lo cual HACE FALTA REVISAR Y SEGURAMENTE 
-    CAMBIAR ALGO EN ESTE MÉTODO. Por que? Pues porque estoy confuso si este 
-    metodo carece de un arraylist que hay que crear para esta clase, o si hay 
-    que llamar al arraylist de TacebookDB(es lo que hice yo) y guardarlo alli, 
-    fijate que como es de otra clase y es un arraylist de otra clase privado, 
-    tuve que utilizar su getter (getProfiles) para entonces llamar al .add
-    y pasar como parametro el profile :)
-    
-    REVISA BIEN ESTO
-    */
-    public static void save(Profile profile){
+    /**
+     * Este método guarda el perfil en el arraylist profile de la clase
+     * TacebookDB, entonces todo lo que tiene que hacer es una llamada al
+     * arraylist con el metodo getter, lo accedemos y añadimos el nuevo
+     * 'profile'
+     *
+     * @param profile perfil del arrayList
+     */
+    public static void save(Profile profile) {
         TacebookDB.getProfiles().add(profile);
     }
-    
-    public static void update(Profile profile){
-    
+
+    /**
+     * Este método de momento no hace nada, dejaré el javadoc añadido para ya
+     * tener esto en cuenta, aun que puede que sea necesario añadir algum param
+     * o return, ya veremos..
+     */
+    public static void update(Profile profile) {
+
     }
-    
+
 }
