0f2540352331ce13e6a3566168aecdc8eaa4458a
SrUsuario <raul.parada040103@gmail.com>
2023-02-01 13:19:36 +0100

    En ProfileController modificado profileView y updateProfileStatus

--- src/tacebook/ProfileController.java
+++ src/tacebook/ProfileController.java
@@ -11,12 +11,8 @@ package tacebook;
  */
 public class ProfileController {
 
-    private ProfileView profileView;
+    private ProfileView profileView = new ProfileView(this);
     private Profile sessionProfile;
-
-    public ProfileController() {
-        profileView = new ProfileView(this);
-    }
 
     /**
      *
@@ -42,11 +38,16 @@ public class ProfileController {
         profileView.showProfileMenu(sessionProfile);
     }
 
+    /*
+    Este método falla al actualizar el estado de un perfil debido a que el perfil es null en los parametros.
+     */
     /**
      *
      * @param newStatus
      */
     public void updateProfileStatus(String newStatus) {
+        //sessionProfile = new Profile(profileView.);
+        //sessionProfile = ProfileDB.findByName(sessionProfile.getName(), getPostsShowed());
         sessionProfile.setStatus(newStatus);
         ProfileDB.save(sessionProfile);
         reloadProfile();
