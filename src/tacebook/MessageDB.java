8b5a45362cd4cdf95b1343cf1d179778523f3a40
SrUsuario <raul.parada040103@gmail.com>
2023-02-02 10:30:47 +0100

    Creada la clase MessageDB con los métodos

---
+++ src/tacebook/MessageDB.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tacebook;
+
+/**
+ *
+ * @author Raúl Parada de la Fuente
+ */
+public class MessageDB {
+
+    public static void save(Message message) {
+        
+    }
+
+    public static void update(Message message) {
+
+    }
+
+    public static void remove(Message message) {
+
+    }
+}
