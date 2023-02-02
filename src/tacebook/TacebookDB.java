8576b5b3919ab93eaeb63e35342c423a982d37c3
SrUsuario <raul.parada040103@gmail.com>
2023-02-02 11:04:03 +0100

    En la clase TacebbokDB traducidos los JavaDocs a castellano

--- src/tacebook/TacebookDB.java
+++ src/tacebook/TacebookDB.java
@@ -10,23 +10,23 @@ import java.util.ArrayList;
  * @author Alejandro Martínez Domínguez, Bilo Alejandro Martins González y Raúl
  * Parada de la Fuente
  *
- * Esta clase garda unicamente un arrayList de obxectos "profile" da clase
- * "Profile". Esta é a clase de persistencia de datos, para almacenar todos os
- * cambios.
+ * Esta clase guarda unicamente un arrayList de objetos "profile" de la clase
+ * "Profile". Esta es la clase de persistencia de datos, para almacenar todos
+ * los cambios.
  */
 public class TacebookDB {
 
     /**
-     * Este é o arraylist que gardará todos os perfiles.
+     * Este es el arraylist que guardará todos los perfiles.
      */
     private static ArrayList<Profile> profiles = new ArrayList<>();
 
     /**
-     * Este método é o getter do arrayList profiles, este método é crucial para
-     * traballar coas outras clases, porque con el vamos sempre facer chamadas a
-     * este arrayList para recibir e entregar datos.
+     * Este método es el getter de arrayList profiles, este método es crucial
+     * para trabajar con las otras clases, porque con el vamos siempre a hacer
+     * llamadas a este arrayList para recibir e entregar datos.
      *
-     * @return o obxecto profile
+     * @return Devolve o objecto profile
      */
     public static ArrayList<Profile> getProfiles() {
         return profiles;
