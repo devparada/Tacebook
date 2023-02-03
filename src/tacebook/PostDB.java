11e845edfabea7874694d31c8a77e51eb1980617
bmargon <bmargon@DAW1-33.dip.local>
2023-02-03 11:55:16 +0100

    Implementado la clase PostDB con sus comentarios.

--- src/tacebook/PostDB.java
+++ src/tacebook/PostDB.java
@@ -5,26 +5,29 @@
 package tacebook;
 
 /**
+ * Esta clase guardará toda la persistencia de datos de las publicaciones
  *
  * @author Bilo Alejandro Martins Gonzalez
  */
 public class PostDB {
 
     /**
+     * Este método permite guardar una publicación y añadirla arriba del todo en
+     * la lista de publiaciones.
      *
      * @param post
      */
     public static void save(Post post) {
-       post.getProfile().getPosts().add(0, post);
-        
+        post.getProfile().getPosts().add(0, post);
     }
 
     /**
+     * Este método guarda un like en la lista de likes de un post.
      *
      * @param post
      * @param profile
      */
     public static void saveLike(Post post, Profile profile) {
-
+        post.getProfileLikes().add(profile);
     }
 }
